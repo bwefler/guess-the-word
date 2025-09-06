190e272731b01698eaac3103b09b46ce872d1f13
Bruce Wefler <bruce.wefler@gmail.com>
2025-09-06 15:18:53 -0600

    Added functionality to display word, guessed letters, and if player won

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const guessButton = document.querySelector(".guess");
 // Text input where player guesses a letter
 const input = document.querySelector("input");
 // Empty paragraph where word in progress appears
-let wordInProgress = document.querySelector("word-in-progress");
+const wordInProgress = document.querySelector(".word-in-progress");
 // Paragraph where remaining guesses display
 const remaining = document.querySelector(".remaining");
 // Span inside paragraph where remaining guesses display
@@ -14,25 +14,27 @@ const span = document.querySelector("span");
 const message = document.querySelector(".message");
 // Hidden button that will appear prompting the player to play again
 const playAgainButton = document.querySelector(".play-again");
+
 // Starting word to test game
 const word = "magnolia";
+// const word = "cat";
+
 // Array to contain all the letters the player guesses
 const guessedLetters = [];
 
 // Function to add placeholders for each letter
 const placeholders = function (guessWord) {
-    let placeArr = []; 
+    const placeholderArr = []; 
     for(let letter of guessWord) {
-        placeArr.push("●");
+        placeholderArr.push("●");
     }
     // console.log(placeArr);
     
     // Merge circles to one "word"
-    circleWord = placeArr.join("");
-    return circleWord;
-}
-// wordInProgress.innerText = placeArr.join("");
-// console.log(wordInProgress);
+    wordInProgress.innerText = placeholderArr.join("");
+};
+placeholders(word);
+
 
 // Event listener for Guess button
 guessButton.addEventListener("click", function (e) {
@@ -45,7 +47,7 @@ guessButton.addEventListener("click", function (e) {
     
     // Variable to capture the value of the input
     inputValue = input.value;
-    console.log(inputValue);
+    // console.log(inputValue);
 
     // Clear value of input
     input.value = "";
@@ -57,7 +59,7 @@ guessButton.addEventListener("click", function (e) {
     // an argument. 
     // Save the result of this function call to a variable and 
     // log it out to the console.
-    let validatedInput = validateInput(inputValue);
+    const validatedInput = validateInput(inputValue);
     //   console.log(`validatedInput: ${validatedInput}`);
 
     // Make sure that the variable mapped to the result of the 
@@ -66,11 +68,11 @@ guessButton.addEventListener("click", function (e) {
     // If it’s returning a letter, pass it as an argument to your 
     // makeGuess function.
     if (validatedInput) {
-        console.log(`validatedInput: ${validatedInput}`);
+        // console.log(`validatedInput: ${validatedInput}`);
         makeGuess(validatedInput);
-    } else {
-        console.log("### Not validated input -- not guessing ###")
-    }
+    } // else {
+      //  console.log("### Not validated input -- not guessing ###")
+    // }
         
 });
 
@@ -91,23 +93,75 @@ const validateInput = function (input) {
     } else {
         return input;
     }
-}
-// console.log(`"" : ${validateInput("")}`);
-// console.log(`aa : ${validateInput("aa")}`);
-// console.log(`9 : ${validateInput("9")}`);
-// console.log(`a : ${validateInput("a")}`);
-// console.log(`Z : ${validateInput("Z")}`);
+};
+
+const showGuessedLetters = function () {
+    guessedLettersElement.innerHTML = "";
+
+    // Create a new list item for each letter inside 
+    // guessedLetters array and add it to the unordered list
+    for (letter of guessedLetters) {
+        const listItem = document.createElement("li");
+        listItem.innerText = letter;
+        guessedLettersElement.append(listItem);
+    }
+};
+
+// Update word in progress; replace ● symbols with correct guesses
+const updateWordInProgress = function (guessedLetters) {
+    // Change to upper case
+    const wordUpper = word.toUpperCase();
+    // split the word string into an array so that the letter 
+    // can appear in the guessedLetters array
+    const wordArray = wordUpper.split("");
+    // console.log(wordArray);
+
+    // Check if the wordArray contains any letters from the 
+    // guessedLetters array. If it does contain any of the 
+    // letters, update the circle symbol with the correct letter.
+    const showWord = [];
+    for (const letter of wordArray) {
+        if (guessedLetters.includes(letter)) {
+            showWord.push(letter.toUpperCase());
+        }
+        else {
+            showWord.push("●");
+        }
+    }
+    wordInProgress.innerText = showWord.join("");
+};
 
 // function to capture input
 const makeGuess = function (letter) {
-    let upperCaseLetter = letter.toUpperCase();
-    console.log(`Letter: ${upperCaseLetter}`);
+    const upperCaseLetter = letter.toUpperCase();
+    // console.log(`Letter: ${upperCaseLetter}`);
     if (guessedLetters.includes(upperCaseLetter)) {
-        console.log(`### ${letter} already guessed ###`);
+        // console.log(`### ${letter} already guessed ###`);
         message.innerText = 
             "You already guessed that letter - try again";
     } else {
         guessedLetters.push(upperCaseLetter);
+        showGuessedLetters();
+        updateWordInProgress(guessedLetters);
+
+        // See if player won 
+        checkForWin(); 
     }
-    console.log(`Guessed letters: ${guessedLetters}`);
-}
+    // console.log(`Guessed letters: ${guessedLetters}`);
+    // console.log(`Word in progress: ${wordInProgress}`);
+};
+
+// Check if player guessed the word and won game
+const checkForWin = function () {
+    
+    // Check if word in progress matches word to be guessed
+    // console.log(`Word in progress: ${wordInProgress.innerText}`);
+    // console.log(`Word: ${word.toUpperCase()}`);
+    if (wordInProgress.innerText === word.toUpperCase()) {
+        // console.log("### We have a winner! ###");
+        message.classList.add("win");
+        message.innerHTML = "";
+        message.innerHTML = '<p class="highlight">You guessed ' +
+            'the correct word! Congrats!</p>';
+    }
+};
